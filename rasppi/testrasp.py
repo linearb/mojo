80c722a7d908fa08649e047aa069240a195e7404
linearb <patrick@mass.fake>
2013-12-07 17:45:19 -0500

    added testrasp.py to test rasp.py

---
+++ rasppi/testrasp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import rasp
+
+def sendsms(tonumber, message):
+    print "test: sending sms to " + tonumber + ": " + message
+
+def sendemail(toaddress, message):
+    print "test: email to " + toaddress + ", message:" + message
+
+# redefine some constants and functions from rasp.py for testing
+rasp.datafile = '../mmdata/incoming.log'
+rasp.pulse = '../mmdata/pulse.d/'
+rasp.sendsms = sendsms
+rasp.sendemail = sendemail
+
+rasp.main()
+
