2ba5579b4fe08be006148d3356eafa6ad945b45d
linearb <patrick@mass.fake>
2013-12-07 17:52:07 -0500

    testrasp.py also tests email alarm now

--- rasppi/testrasp.py
+++ rasppi/testrasp.py
@@ -11,6 +11,8 @@ def sendemail(toaddress, message):
 # redefine some constants and functions from rasp.py for testing
 rasp.datafile = '../mmdata/incoming.log'
 rasp.pulse = '../mmdata/pulse.d/'
+rasp.should_sendemail = True
+rasp.should_sendsms = True
 rasp.sendsms = sendsms
 rasp.sendemail = sendemail
 
